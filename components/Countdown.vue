ce982068e097f500f50ecacf56272443c1a4b3f6
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-07 09:24:30 -0300

    Create new components
    Components for countdown timer and the header itself are created.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ components/Countdown.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="circle days">
+      <!-- <p class="digits">{{ days }}</p> -->
+      <p>Testing</p>
+      <p class="text">Days</p>
+    </div>
+    <div class="circle hours">
+      <!-- <p class="digits">{{ hours }}</p> -->
+      <p>testing</p>
+      <p class="text">Hours</p>
+    </div>
+    <div class="circle minutes">
+      <!-- <p class="digits">{{ minutes }}</p> -->
+      <p>testing</p>
+      <p class="text">Minutes</p>
+    </div>
+    <div class="circle seconds">
+      <!-- <p class="digits">{{ seconds }}</p> -->
+      <p>testing</p>
+      <p class="text">Seconds</p>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:400|Roboto:100);
+
+.circle {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+}
+
+.text {
+  color: #1abc9c;
+  font-size: 40px;
+  font-family: "Roboto Condensed", serif;
+  font-weight: 40;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.digits {
+  color: #30cff7;
+  font-size: 150px;
+  font-weight: 100;
+  font-family: "Roboto", serif;
+  margin: 10px;
+  text-align: center;
+}
+</style>
+
+<script>
+// export default {
+//   ready() {
+//     window.setInterval(() => {
+//       this.now = Math.trunc(new Date().getTime() / 1000);
+//     }, 1000);
+//   },
+//   data() {
+//     return {
+//       now: Math.trunc(new Date().getTime() / 1000),
+//       date: new Date("June 06 2018")
+//     };
+//   },
+
+//   computed: {
+//     now: function() {
+//       window.setInterval(() => {
+//         Math.trunc(new Date().getTime() / 1000);
+//       }, 1000);
+//     },
+//     seconds() {
+//       return (this.date - this.now()) % 60;
+//     },
+
+//     minutes() {
+//       return Math.trunc((this.date - this.now()) / 60) % 60;
+//     },
+
+//     hours() {
+//       return Math.trunc((this.date - this.now()) / 60 / 60) % 24;
+//     },
+
+//     days() {
+//       return Math.trunc((this.date - this.now()) / 60 / 60 / 24);
+//     }
+//   }
+// };
+</script>
---
+++ components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="header">
+    <h1>HackathonUSP 2018</h1>
+    <countdown/>
+  </section>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding-top: 4em;
+}
+</style>
+
+<script>
+import Countdown from "~/components/Countdown.vue";
+export default {
+  components: {
+    Countdown
+  }
+};
+</script>
