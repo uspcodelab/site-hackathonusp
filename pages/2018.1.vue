3d8c0f9fd88cb0e32ba099f447d3551d27a9c2ce
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-12 16:54:50 -0300

    Create new components
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- pages/2018.1.vue
+++ pages/2018.1.vue
@@ -2,6 +2,12 @@
   <div class="page">
     <b-container fluid>
       <the-header/>
+      <!-- <main>
+        <about/>
+        <links/>
+        <prizes/>
+        <sponsors/>
+      </main> -->
       <footer/>
     </b-container>
   </div>
@@ -11,12 +17,22 @@
 import Countdown from "~/components/Countdown.vue";
 import TheHeader from "~/components/TheHeader.vue";
 import Footer from "~/components/Footer.vue";
+// import About from "~/components/About.vue";
+// import Links from "~/components/Links.vue ";
+// import Prizes from "~/components/Prizes.vue ";
+// import Sponsors from "~/components/Sponsors.vue ";
+// import PastEvents from "~/components/PastEvents.vue ";
 
 export default {
   components: {
     Countdown,
     TheHeader,
     Footer
+    // About,
+    // Links,
+    // Prizes,
+    // Sponsors,
+    // PastEvents
   }
 };
 </script>
