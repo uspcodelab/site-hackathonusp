ab3a7c5520b0205af3afa9709932db06bcde0d67
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-17 10:04:04 -0300

    Center HackathonUSP 2018
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,7 @@
 <template>
-  <!-- <section class="header">
-    <h1>HackathonUSP 2018</h1>
-    <countdown />
-  </section> -->
   <b-row id="header" tag="section" class="header p-3 p-sm-5">
     <b-col sm="12" class="header__text">
-      <h1 class="text-center text-md-left pb-3">
+      <h1 class="text-center pb-3">
         HackathonUSP 2018
       </h1>
     </b-col>
@@ -20,7 +16,7 @@
   justify-content: center;
   min-height: 100vh;
   align-items: center;
-  align-self: center;
+  // align-self: center;
   text-align: center;
   padding-top: 4em;
   background-color: #bce4f5;
