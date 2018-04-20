53ace83085c6443c2c696d08a42446907051ac5f
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-19 21:40:20 -0300

    Add HackathonUSP2018 and Tema links and adjust font-size
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,8 @@
     <b-navbar-toggle target="nav_dropdown_collapse"/>
     <b-collapse id="nav_dropdown_collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item v-scroll-to="'#about'">HackathonUSP</b-nav-item>
+        <b-nav-item v-scroll-to="'#header'">HackathonUSP 2018</b-nav-item>
+        <b-nav-item v-scroll-to="'#about'">Tema</b-nav-item>
         <b-nav-item v-scroll-to="'#prizes'">Prêmios</b-nav-item>
         <b-nav-item v-scroll-to="'#sponsors'">Patrocinadores</b-nav-item>
         <b-nav-item v-scroll-to="'#footer'">Contato</b-nav-item>
@@ -19,9 +20,10 @@
 .navbar {
   box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
   min-height: 4rem;
+  font-size: 1em;
 
   &__logo {
-    height: 3rem;
+    height: 4rem;
   }
 }
 </style>
