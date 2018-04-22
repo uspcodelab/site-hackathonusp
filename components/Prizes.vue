396ce17cae86dc5bdf241643c43ccfe9474310ef
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-04-22 18:12:10 -0300

    Add content and change awards in the prizes section
    Refactor cards to present the positions as the cards' titles. Add
    special plus sign to indicate multiple prizes.
    
    Signed-off-by: Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>

--- components/Prizes.vue
+++ components/Prizes.vue
@@ -1,48 +1,62 @@
 <template>
-  <b-row id="prizes" tag="section" class="px-3 py-5 px-sm-5">
-    <b-col sm="12" class="py-5">
-      <h1 class="text-center pb-3">
-        Prêmios
-      </h1>
-    </b-col>
-    <b-col sm="12" class=" py-3 p-md-2">
-      <b-card border-variant="light"
-              img-bottom>
-        <h3 class="text-center">1˚ Lugar</h3>
-        <p class="text-center">
-          Viagem para Palo Alto com tudo pago + Curso ministrado na Universidade
-          de Stanford
-        </p>
-      </b-card>
+  <b-row id="prizes" tag="section" class="prizes px-3 px-sm-5 py-5">
+
+    <b-col sm="12" class="text-center py-5">
+      <h1 class="pb-3">Prêmios</h1>
+      <p class="text-center">
+        Os membros dos times mais bem colocados nos nossos
+        <strong>critérios de julgamento</strong> ganharão
+        cada um os seguintes prêmios:
+      </p>
     </b-col>
 
-    <b-col sm="12" md="12" lg="6" class=" py-3 p-md-2">
-      <b-card body-bg-variant="light" class="text-center">
-        <h3>2˚ Lugar</h3>
-        Grupos de estudo com reuniões semanais, ao longo
-        de um semestre, para aplicar os conhecimentos
-        em projetos propostos pela organização
+    <b-col cols="12" class="py-3 p-md-2">
+      <b-card border-variant="light"
+              text-variant="dark"
+              class="text-center"
+              title="1˚ Lugar">
+        Intercâmbio para o <strong>Vale do Silício</strong>
+        financiado pela USP
+        <!-- <br><i class="text&#45;secondary fa fa&#45;plus"/><br> -->
+        <!-- Curso de curta duração ministrado -->
+        <!-- na <strong>Universidade de Stanford</strong> -->
+        <br><i class="text-secondary fa fa-plus"/><br>
+        <strong>Troféu</strong> do HackathonUSP
+        <br><i class="text-secondary fa fa-plus"/><br>
+        <strong>Medalha</strong> do HackathonUSP
       </b-card>
     </b-col>
 
     <b-col sm="12" md="12" lg="6" class="py-3 p-md-2">
-      <b-card body-bg-variant="light" class="text-center">
-        <h3>3˚ Lugar</h3>
-        Grupo de estudos avançado, com projetos em times
-        para aprendizado de técnicas de modelagem,
-        implementação e manutenção de sistemas
+      <b-card border-variant="light"
+              text-variant="dark"
+              class="text-center"
+              title="2˚ Lugar">
+        Kindle
+        <br><i class="text-secondary fa fa-plus"/><br>
+        <strong>Medalha</strong> do HackathonUSP
       </b-card>
     </b-col>
+
+    <b-col sm="12" md="12" lg="6" class="py-3 p-md-2">
+      <b-card border-variant="light"
+              class="text-center"
+              text-variant="dark"
+              title="3˚ Lugar">
+        Fone de ouvido
+        <br><i class="text-secondary fa fa-plus"/><br>
+        <strong>Medalha</strong> do HackathonUSP
+      </b-card>
+    </b-col>
+
   </b-row>
 </template>
 
 <style lang="scss">
-#prizes {
-  background-color: #00b2ff;
-}
+.prizes {
+  min-height: calc(100vh - #{$navbar-height});
 
-h1 {
-  color: #fff;
-  font-size: 4em;
+  color: theme-color("light");
+  background-color: theme-color("secondary");
 }
 </style>
