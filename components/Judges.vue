fceec79da60135544c4f1ef3bd78ba9acc5823c3
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-04-25 17:24:32 -0300

    Update judge components to prepare them for content
    It is still necessary to add their descriptions and profile pictures.
    
    Signed-off-by: Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>

--- components/Judges.vue
+++ components/Judges.vue
@@ -1,77 +1,71 @@
 <template>
-  <b-row id="judges" tag="section" class="judges px-3 px-sm-5 py-5">
-    <b-col sm="12" class="text-center px-lg-5 py-5">
+  <b-row id="judges" tag="section" class="judges text-center px-3 px-sm-5 py-5">
+
+    <b-col sm="12" class="px-lg-5 py-5">
       <h1 class="text-primary pb-3">Jurados</h1>
     </b-col>
-    <b-col sm="3" class="text-center py-3 py-md-0">
-      <img src="~/assets/round-placeholder.svg" alt="placeholder" style="width: 60%; height: auto">
-    </b-col>
-    <b-col sm="3" class="py-3 py-md-0">
-      <p><strong>Denis Mauá</strong></p>
+
+    <b-col sm="4" class="judges__profile py-3 py-md-0">
+      <img src="@/assets/round-placeholder.svg" alt="placeholder"
+           class="w-50 py-3">
+      <strong>Denis Mauá</strong>
       <p>
-        Denis Mauá is an Assistant Professor of Computer Science at the Institute of Mathematics and Statistics of the University of São Paulo.
-        He Obtained his PhD from University of Lugano, Switzerland. 
-        His research interests lie in the fields of probabilistic reasoning, and its application to artificial intelligence and machine learning.
+        Em breve...
       </p>
     </b-col>
-    <b-col sm="3" class="text-center py-3 py-md-0">
-      <img src="~/assets/round-placeholder.svg" alt="placeholder" style="width: 60%; height: auto">
-    </b-col>
-    <b-col sm="3" class="py-3 py-md-0">
-      <p><strong>Judge Name</strong></p>
+
+    <b-col sm="4" class="judges__profile py-3 py-md-0">
+      <img src="@/assets/round-placeholder.svg" alt="placeholder"
+           class="w-50 py-3">
+      <strong>Vitor Paulo</strong>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Doloribus tempora dignissimos velit repellat eligendi, 
-        sequi exercitationem, cumque, pariatur ducimus labore 
-        debitis at iure facilis quia eum minima illo veritatis. 
-        Sequi.
+        Em breve...
       </p>
     </b-col>
-    <b-col sm="3" class="text-center py-3 py-md-0">
-      <img src="~/assets/round-placeholder.svg" alt="placeholder" style="width: 60%; height: auto">
-    </b-col>
-    <b-col sm="3" class="py-3 py-md-0">
-      <p><strong>Judge Name</strong></p>
+
+    <b-col sm="4" class="judges__profile py-3 py-md-0">
+      <img src="@/assets/round-placeholder.svg" alt="placeholder"
+           class="w-50 py-3">
+      <strong>Nina Hirata</strong>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Doloribus tempora dignissimos velit repellat eligendi, 
-        sequi exercitationem, cumque, pariatur ducimus labore 
-        debitis at iure facilis quia eum minima illo veritatis. 
-        Sequi.
+        Em breve...
       </p>
     </b-col>
-    <b-col sm="3" class="text-center py-3 py-md-0">
-      <img src="~/assets/round-placeholder.svg" alt="placeholder" style="width: 60%; height: auto">
-    </b-col>
-    <b-col sm="3" class="py-3 py-md-0">
-      <p><strong>Judge Name</strong></p>
+
+    <b-col sm="4" offset-sm="2" class="judges__profile py-3 py-md-0">
+      <img src="@/assets/round-placeholder.svg" alt="placeholder"
+           class="w-50 py-3">
+      <strong>Isa Kobuto</strong>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Doloribus tempora dignissimos velit repellat eligendi, 
-        sequi exercitationem, cumque, pariatur ducimus labore 
-        debitis at iure facilis quia eum minima illo veritatis. 
-        Sequi.
+        Em breve...
       </p>
     </b-col>
-    <b-col sm="6" class="text-center py-3 py-md-0">
-      <img src="~/assets/round-placeholder.svg" alt="placeholder" style="width: 30%; height: auto">
-    </b-col>
-    <b-col sm="6" class="py-5 py-md-0">
-      <p><strong>Judge Name</strong></p>
+
+    <b-col sm="4" class="judges__profile py-5 py-md-0">
+      <img src="@/assets/round-placeholder.svg" alt="placeholder"
+           class="w-50 py-3">
+      <strong>Renato Cordeiro</strong>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Doloribus tempora dignissimos velit repellat eligendi, 
-        sequi exercitationem, cumque, pariatur ducimus labore 
-        debitis at iure facilis quia eum minima illo veritatis. 
-        Sequi.
+        Em breve...
       </p>
     </b-col>
-  </b-row>  
+  </b-row>
 </template>
 
 <style lang="scss">
+// Dênis Mauá:
+// Professor Asistente de Ciência da Computação do IME-USP.
+// Pesquisa nos campos de raciocínio probabilistico e suas aplicações
+// em inteligênica artificial e aprendizagem de máquina
+
 .judges {
   min-height: calc(100vh - #{$navbar-height});
   background-color: theme-color("light");
+
+  &__profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
