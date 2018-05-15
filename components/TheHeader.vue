2fb8ac679419bc109ade2a7188edaf5660a6422e
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-15 18:33:51 -0300

    Fix bug with new Opera
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,17 +4,17 @@
       <img src="~/assets/hackathon-usp.svg" class="w-100">
     </b-col>
     <b-col sm="7" lg="6" class="header__text py-3 py-md-5">
-      <h1 class="text-center text-md-left pb-3">
+      <h1 class="text-center text-md-left pb-3 w-100">
         HackathonUSP 2018
       </h1>
-      <p class="lead text-center text-md-left">
+      <p class="lead text-center text-md-left w-100">
         Aplicando <strong>ciência de dados</strong> para melhorar
         <span class="text-md-nowrap">o dia a dia universitário</span>
       </p>
-      <p class="text-center text-md-left">
+      <p class="text-center text-md-left w-100">
         9 e 10 de junho de 2018
       </p>
-      <p class="text-center text-md-left">
+      <p class="text-center text-md-left w-100">
         Instituto de Matemática e Estatística da USP
       </p>
       <button v-if="subsGap" class="btn header__subs">Inscreva-se!</button>
