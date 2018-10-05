5a4f0f9bf12a4a7a5bbb5be9e008dc89782b2c47
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-10-05 13:30:50 -0300

    Lint fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "A maior competição de desenvolvimento da USP" }
+      {
+        hid: "description",
+        name: "description",
+        content: "A maior competição de desenvolvimento da USP"
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/icon.png" },
